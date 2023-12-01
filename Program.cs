8c46d5323afbd012a1ef24bec3246296c3761a91
rfranett <rfranett@gmail.com>
2023-12-01 10:05:26 -0300

    atualizado

--- Program.cs
+++ Program.cs
@@ -11,6 +11,12 @@ else if (letra == "e" || letra =="i" || letra == "o" || letra == "u")
 {
     Console.WriteLine("Vogal");
 }
+else if (letra == "b" || letra =="c" || letra == "d" || letra == "f" || letra == "g" || letra == "h" || letra == "j" || letra == "k" || letra == "l"
+ || letra == "m" || letra == "n" || letra == "p" || letra == "q" || letra == "r" || letra == "s" || letra == "t" || letra == "v" || letra == "w"
+  || letra == "x" || letra == "y" || letra == "z")
+{
+    Console.WriteLine("Consoantes");
+}
 else
 {
     Console.WriteLine("Não é uma Vogal");
