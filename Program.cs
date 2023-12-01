30ad123f1b1e228a7f1f49cd15fed7c066938bc9
rfranett <rfranett@gmail.com>
2023-12-01 09:32:09 -0300

    Vogais

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using project.net9.models ;
+
+Console.WriteLine("Digite uma Letra");
+string letra = Console.ReadLine();
+
+if (letra == "a")
+{
+    Console.WriteLine("Vogal");
+}
+else if (letra == "e" || letra =="i" || letra == "o" || letra == "u")
+{
+    Console.WriteLine("Vogal");
+}
+else
+{
+    Console.WriteLine("Não é uma Vogal");
+}
